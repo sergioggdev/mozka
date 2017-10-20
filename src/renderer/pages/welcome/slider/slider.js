c07347af9eda692844d78efbd16cccbb7346fbfc
Sergio Garcia <sergio.garcia.seo@gmail.com>
2017-10-20 17:10:25 +0200

    Terminar pantalla welcome

--- src/renderer/pages/welcome/slider/slider.js
+++ src/renderer/pages/welcome/slider/slider.js
@@ -2,16 +2,72 @@ import React, { Component } from 'react';
 import './slider.scss';
 
 export default class Slider extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: null,
+            content: null,
+            background: null, 
+            icon: null,
+            animationClass: '',
+            backgroundClass: '',
+        };
+    }
+
+    componentWillMount() {
+        this.setState({
+            title: this.props.title,
+            content: this.props.content,
+            background: this.props.background, 
+            icon: this.props.icon,
+        });
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.animation === true) {
+            this.setState({animationClass: 'animationNextIn'});
+			setTimeout( () => {
+				this.setState({
+                    title: this.props.title,
+                    content: this.props.content,
+                    background: this.props.background, 
+                    icon: this.props.icon,
+                    animationClass: 'animationNextOut'
+                });
+				setTimeout( () => {
+					this.setState({animationClass: ''});
+				},500);
+			},500);
+        } else if (nextProps.animation === false) {
+            this.setState({animationClass: 'animationBackIn'});
+			setTimeout( () => {
+				this.setState({
+                    title: this.props.title,
+                    content: this.props.content,
+                    background: this.props.background, 
+                    icon: this.props.icon,
+                    animationClass: 'animationBackOut'
+                });
+				setTimeout( () => { 
+					this.setState({animationClass: ''});
+				},500);
+			},500);
+        }
+    }
+
+    componentWillUpdate() {
+        //console.log('se actualiza');
+    }
 
 	render() {
 		return (			
             <div className="slide">
-                <div className="slide__background" style={this.props.background} />
+                <div className="slide__background" style={this.state.background} />
                 <div className="slide__background-gradient" />
-                <div className="slide__area-text">
-                    <img src={this.props.icon}/>
-                    <h3>{this.props.title}</h3>
-                    <p>{this.props.content}</p>
+                <div className={`slide__area-text ${this.state.animationClass}`}>
+                    <img src={this.state.icon}/>
+                    <h3>{this.state.title}</h3>
+                    <p>{this.state.content}</p>
                 </div>
             </div>   
 		);
--- src/renderer/pages/welcome/welcome.js
+++ src/renderer/pages/welcome/welcome.js
@@ -3,42 +3,86 @@ import Slider from './slider/slider';
 import './welcome.scss';
 
 import Background from './slider/fondo.jpg';
+import Background1 from './slider/fondo1.jpg';
 import Icon from './slider/icon.png';
+
+const slides = [
+	{
+		id: 1,
+		title: "See how it works",
+		content: 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró',
+		background: { backgroundImage: `url(${Background}` },
+		icon: Icon
+	}, 
+	{
+		id: 2,
+		title: "Works Perfectily",
+		content: 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró',
+		background: { backgroundImage: `url(${Background}` },
+		icon: Icon
+	}, 
+	{
+		id: 3,
+		title: "Amazing!",
+		content: 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró',
+		background: { backgroundImage: `url(${Background1}` },
+		icon: Icon
+	}
+];
 
 export default class Welcome extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {
-			activeSlide: 0
-		};
+		this.wait = false;
 		this.nextSlide = this.nextSlide.bind(this);
 		this.backSlide = this.backSlide.bind(this);
+		this.state = {
+			activeSlide: 0,
+			animation: null,
+		};
+		
 	}
 
 	nextSlide() {
-		this.setState({activeSlide: this.state.activeSlide + 1});
+		if (!this.wait) {
+			if (slides.length-1 > this.state.activeSlide) {
+				this.wait = true;
+				this.setState({activeSlide: this.state.activeSlide + 1, animation: true});
+				setTimeout( () => { this.wait = false; }, 1100);	
+			}
+			if (slides.length-1 === this.state.activeSlide) {
+				alert('Felicidades, terminaste el tutorial');
+			}
+		}
 	}
+
 	backSlide() {
-		this.setState({activeSlide: this.state.activeSlide - 1});
+		if (!this.wait) {
+			if (this.state.activeSlide > 0) {
+				this.wait = true;
+				this.setState({activeSlide: this.state.activeSlide - 1, animation: false});
+				setTimeout( () => { this.wait = false; }, 1100);	
+			}
+		}
 	}
 
 	render() {
+		let siguiente = slides.length-1 === this.state.activeSlide ? 'Terminar' : 'Siguiente'; 
 		return (
 			<div className="welcome">
-				
 				<Slider 
 					title={slides[this.state.activeSlide].title} 
 					content={slides[this.state.activeSlide].content} 
 					background={slides[this.state.activeSlide].background} 
-					icon={slides[this.state.activeSlide].icon} 
+					icon={slides[this.state.activeSlide].icon}
+					animation={this.state.animation}
 				/>
-		
 				<div className="welcome__navigator">
 					<div onClick={this.backSlide}>Atras</div>
 					<div className="welcome__navigator__balls">
-						{slides.map( i => <span>O </span> )}
+						{slides.map( i => <span key={i.id}>O </span> )}
 					</div>
-					<div onClick={this.nextSlide}>Siguiente</div>
+					<div onClick={this.nextSlide}>{siguiente}</div>
 				</div>
 				
 			</div>
@@ -46,23 +90,3 @@ export default class Welcome extends Component {
 	}
 }
 
-const slides = [
-	{
-		title: "See how it works",
-		content: 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró',
-		background: { backgroundImage: `url(${Background}` },
-		icon: Icon
-	}, 
-	{
-		title: "Works Perfectily",
-		content: 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró',
-		background: { backgroundImage: `url(${Background}` },
-		icon: Icon
-	}, 
-	{
-		title: "Amazing!",
-		content: 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró',
-		background: { backgroundImage: `url(${Background}` },
-		icon: Icon
-	}
-];
