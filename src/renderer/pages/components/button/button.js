8468d6e69cadc95bbbf87ac45c8890b3dc9cd62a
Juan Antonio Varea <javarea@eservicios.indra.es>
2017-10-24 11:37:50 +0200

    boton con lineas

--- src/renderer/pages/components/button/button.js
+++ src/renderer/pages/components/button/button.js
@@ -10,11 +10,12 @@ export default class Button extends Component {
             // </div>
             <div class="svg-wrapper">
             <svg height="60" width="320" xmlns="http://www.w3.org/2000/svg">
-              <rect class="shape" height="60" width="320" />
-              {/* {<line class="top" x1="0" y1="0" x2="320" y2="0"></line>
-              <line class="left" x1="320" y1="0" x2="320" y2="60"></line>
-              <line class="bottom" x1="300" y1="460" x2="-600" y2="460"></line>
-              <line class="right" x1="300" y1="0" x2="300" y2="1380"></line>} */}
+              {/* <rect class="shape" height="60" width="320" /> */}
+                                 {/*p ancho    p alto       f ancho     f alto*/}
+              <line class="top"      x1="0"     y1="0"      x2="320"    y2="0"></line>
+              <line class="left"     x1="0"     y1="0"      x2="0"      y2="60"></line>
+              <line class="bottom"   x1="0"     y1="60"     x2="320"    y2="60"></line>
+              <line class="right"    x1="320"   y1="0"      x2="320"    y2="60"></line>}
             </svg>
              <div class="text">Nuevo</div>
           </div>
