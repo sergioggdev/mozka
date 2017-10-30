69a8143510bd7168925b40124287cecb50db42b4
Sergio Garcia <sergio.garcia.seo@gmail.com>
2017-10-30 11:53:48 +0100

    Crear componentes modal

---
+++ src/renderer/pages/components/modal/modal.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import './modal.scss';
+
+export default class Modal extends Component {
+
+    render() {
+        return(
+            <div className="modal">
+
+            </div>
+        );
+    }
+} 
+
+Modal.propTypes = {
+    
+};
