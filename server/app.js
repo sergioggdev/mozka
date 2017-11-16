726410abdc24c830e0b8521cea73aa801f68489c
Sergio Garcia <sergio.garcia.seo@gmail.com>
2017-11-16 02:16:21 +0100

    configuracion server y websockets

--- server/app.js
+++ server/app.js
@@ -1,146 +1,70 @@
 const path = require('path');
 const http = require('http');
 const express = require('express');
-const io = require('socket.io');
-const debug = require('debug')('servidor:server');
-const favicon = require('serve-favicon');
+const WebSocket = require('ws');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const index = require('./routes/index');
 
-const app = express();
-
+// Comunicacion con electron
 process.on('message', (msg) => {
     console.log('padre dice: ', msg);
     process.send('Hola papa!');
 });
 
-/* io.on('connection', function(socket){
-  console.log('a user connected');
-}); */
-
-
-// view engine setup
+// Configuracion del Servidor
+const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-// uncomment after placing your favicon in /public
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.set('port', Number(process.env.PORT) || 3000);
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+// Importacion del Router
 app.use('/', index);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+// capturar 404 y enviar el errorHandler
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// errorHandler
+app.use((err, req, res) => {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
-module.exports = app;
 
-
-
-
-
-
-
-/**
- * Get port from environment and store in Express.
- */
-
-var port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
-
-/**
- * Create HTTP server.
- */
-
+// Create HTTP server.
 const server = http.createServer(app);
-const socket = io(server);
-socket.on('connection', (socket) => {
-    console.log('a user connected');
+server.listen(app.get('port'));
+server.on('error', (err) => {
+    if (err.code === 'EACCES') {
+        console.error(`El puerto ${app.get('port')} requiere privilegios`);
+        process.exit(1);
+    } else if (err.code === 'EADDRINUSE') {
+        console.error(`El puerto ${app.get('port')} ya esta en uso`);
+        // process.exit(1);
+    }
+    throw err;
 });
-/**
- * Listen on provided port, on all network interfaces.
- */
 
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
+// Conexion al sokets
+const socket = new WebSocket.Server({ server });
+socket.on('connection', (ws) => {
+    console.log('a user connected');
+    ws.on('message', (msg) => {
+        console.log(msg);
+    });
+    ws.send('Hola, te envio un mensaje por websokets');
+});
 
-/**
- * Normalize a port into a number, string, or false.
- */
-
-function normalizePort(val) {
-  var port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
-
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-
-  return false;
-}
-
-/**
- * Event listener for HTTP server "error" event.
- */
-
-function onError(error) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-
-  var bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-}
-
-/**
- * Event listener for HTTP server "listening" event.
- */
-
-function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  debug('Listening on ' + bind);
-}
